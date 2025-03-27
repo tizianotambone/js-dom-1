129de44620314581278556c2f4cb61e8245145c6
tizianotambone <tambonetiziano@gmail.com>
2025-03-27 16:33:33 +0100

    [UPD] ex done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// richiamo gli elementi da selezionare
+
 const immagine = document.getElementById("lampadaBianca")
 const bottone =document.getElementById("button")
+// creo la funzione che mi permette di inseire la lampada gialla al push del tasto accendi
 
 bottone.addEventListener(`click`, function(){
     immagine.src="./img/yellow_lamp.png"
